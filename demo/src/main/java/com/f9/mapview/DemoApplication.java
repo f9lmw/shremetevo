87bd8d185fe7d878cc556d827163f89d0465f193
f9lmw <82672019+f9lmw@users.noreply.github.com>
2021-05-23 10:32:46 +0300

    Add files via upload

---
+++ demo/src/main/java/com/f9/mapview/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.f9.mapview;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+
+public class DemoApplication extends Application {
+
+    private static Context context;
+    private RefWatcher refWatcher;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        refWatcher = LeakCanary.install(this);
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
